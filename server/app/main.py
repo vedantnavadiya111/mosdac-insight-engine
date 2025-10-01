cfb42d9ad49ca475969b7a55b85f73562b6b112c
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-10-02 02:57:47 +0530

    CORS-update

--- server/app/main.py
+++ server/app/main.py
@@ -6,13 +6,23 @@ app = FastAPI(title="MOSDAC Bot Backend")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-    ],
+    allow_origins=["*"],  # Allow all origins for development
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=[
+        "GET",
+        "POST",
+        "PUT",
+        "DELETE",
+        "OPTIONS",
+    ],  # Explicitly list methods
+    allow_headers=[
+        "Content-Type",
+        "Authorization",
+        "Access-Control-Allow-Origin",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Methods",
+        "Access-Control-Allow-Credentials",
+    ],
 )
 
 
